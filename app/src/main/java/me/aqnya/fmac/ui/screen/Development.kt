bf2a0bd86a1a3985f8b2c12762cdda0d163bd03f
Nyawww <siyueba73@gmail.com>
2025-08-04 18:16:55 +0800

    add dev mode.

--- app/src/main/java/me/aqnya/fmac/ui/screen/Development.kt
+++ app/src/main/java/me/aqnya/fmac/ui/screen/Development.kt
@@ -1,5 +1,25 @@
 package me.aqnya.fmac.ui.screen
 
+import android.content.Context
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.DeveloperMode
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import me.aqnya.fmac.R
+
 @Destination<RootGraph>
 @Composable
 fun DeveloperModeScreen(navigator: DestinationsNavigator) {
