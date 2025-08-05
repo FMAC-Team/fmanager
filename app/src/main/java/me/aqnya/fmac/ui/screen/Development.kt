f82ab2897db29ebdbd40f1c6aef0f25cbff09faa
Nyawww <siyueba73@gmail.com>
2025-08-05 11:21:58 +0800

    add fake version disable

--- app/src/main/java/me/aqnya/fmac/ui/screen/Development.kt
+++ app/src/main/java/me/aqnya/fmac/ui/screen/Development.kt
@@ -6,8 +6,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.Modifier.alpha
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.DeveloperMode
