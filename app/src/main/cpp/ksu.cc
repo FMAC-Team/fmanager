6d05179ccbca40d47b73e28042289df8b4dfae84
Nyawww <siyueba73@gmail.com>
2025-08-05 17:59:28 +0800

    add is_root native.

--- app/src/main/cpp/ksu.cc
+++ app/src/main/cpp/ksu.cc
@@ -2,10 +2,10 @@
 // Created by weishu on 2022/12/9.
 //
 
-#include <sys/prctl.h>
 #include <stdint.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/prctl.h>
 #include <unistd.h>
 
 #include "ksu.h"
@@ -30,78 +30,83 @@
 #define CMD_IS_SU_ENABLED 14
 #define CMD_ENABLE_SU 15
 
-static bool ksuctl(int cmd, void* arg1, void* arg2) {
-    int32_t result = 0;
-    prctl(KERNEL_SU_OPTION, cmd, arg1, arg2, &result);
-    return result == KERNEL_SU_OPTION;
+static bool ksuctl(int cmd, void *arg1, void *arg2) {
+  int32_t result = 0;
+  prctl(KERNEL_SU_OPTION, cmd, arg1, arg2, &result);
+  return result == KERNEL_SU_OPTION;
 }
 
-bool is_root(){
-if (getuid()==0){
-return true;
-}else{
-return false;
-}}
-
-bool become_manager(const char* pkg) {
-    char param[128];
-    uid_t uid = getuid();
-    uint32_t userId = uid / 100000;
-    if (userId == 0) {
-        sprintf(param, "/data/data/%s", pkg);
+bool is_root() {
+  if (!is_fake) {
+    if (getuid() == 0) {
+      return true;
     } else {
-        snprintf(param, sizeof(param), "/data/user/%d/%s", userId, pkg);
+      return false;
     }
+  } else {
+    return true;
+  }
+}
 
-    return ksuctl(CMD_BECOME_MANAGER, param, nullptr);
+bool become_manager(const char *pkg) {
+  char param[128];
+  uid_t uid = getuid();
+  uint32_t userId = uid / 100000;
+  if (userId == 0) {
+    sprintf(param, "/data/data/%s", pkg);
+  } else {
+    snprintf(param, sizeof(param), "/data/user/%d/%s", userId, pkg);
+  }
+
+  return ksuctl(CMD_BECOME_MANAGER, param, nullptr);
 }
 
 // cache the result to avoid unnecessary syscall
 static bool is_lkm = false;
 
 int get_version(void) {
-    int32_t version = -1;
-    int32_t flags = 0;
-    ksuctl(CMD_GET_VERSION, &version, &flags); 
-    if (!is_lkm && (flags & 0x1)) {
-        is_lkm = true;
-    }
-    return version;
+  int32_t version = -1;
+  int32_t flags = 0;
+  ksuctl(CMD_GET_VERSION, &version, &flags);
+  if (!is_lkm && (flags & 0x1)) {
+    is_lkm = true;
+  }
+  return version;
 }
 
 bool get_allow_list(int *uids, int *size) {
-    return ksuctl(CMD_GET_SU_LIST, uids, size);
+  return ksuctl(CMD_GET_SU_LIST, uids, size);
 }
 
-bool is_safe_mode() {
-    return ksuctl(CMD_CHECK_SAFEMODE, nullptr, nullptr);
-}
+bool is_safe_mode() { return ksuctl(CMD_CHECK_SAFEMODE, nullptr, nullptr); }
 
 bool is_lkm_mode() {
-    // you should call get_version first!
-    return is_lkm;
+  // you should call get_version first!
+  return is_lkm;
 }
 
 bool uid_should_umount(int uid) {
-    bool should;
-    return ksuctl(CMD_IS_UID_SHOULD_UMOUNT, reinterpret_cast<void*>(uid), &should) && should;
+  bool should;
+  return ksuctl(CMD_IS_UID_SHOULD_UMOUNT, reinterpret_cast<void *>(uid),
+                &should) &&
+         should;
 }
 
 bool set_app_profile(const app_profile *profile) {
-    return ksuctl(CMD_SET_APP_PROFILE, (void*) profile, nullptr);
+  return ksuctl(CMD_SET_APP_PROFILE, (void *)profile, nullptr);
 }
 
 bool get_app_profile(p_key_t key, app_profile *profile) {
-    return ksuctl(CMD_GET_APP_PROFILE, (void*) profile, nullptr);
+  return ksuctl(CMD_GET_APP_PROFILE, (void *)profile, nullptr);
 }
 
 bool set_su_enabled(bool enabled) {
-    return ksuctl(CMD_ENABLE_SU, (void*) enabled, nullptr);
+  return ksuctl(CMD_ENABLE_SU, (void *)enabled, nullptr);
 }
 
 bool is_su_enabled() {
-    bool enabled = true;
-    // if ksuctl failed, we assume su is enabled, and it cannot be disabled.
-    ksuctl(CMD_IS_SU_ENABLED, &enabled, nullptr);
-    return enabled;
+  bool enabled = true;
+  // if ksuctl failed, we assume su is enabled, and it cannot be disabled.
+  ksuctl(CMD_IS_SU_ENABLED, &enabled, nullptr);
+  return enabled;
 }
