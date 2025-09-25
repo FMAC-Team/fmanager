12e39c98ef893cc6833a881bec44dedca98325e5
喵呜 <153448995+aqnya@users.noreply.github.com>
2025-09-25 13:07:10 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:animations/animations.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'status.dart';
